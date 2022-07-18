8556abc0f337a573ede5bfea8b825a6b7bd13ec2
nechel11 <zaf-khasanov@yandex.ru>
2022-07-18 11:27:29 +0300

    added long handle

--- internal/database.go
+++ internal/database.go
@@ -20,15 +20,62 @@ func Db_connect() *sql.DB{
 	psqconn := fmt.Sprintf("host= %s port = %d user = %s password = %s dbname = %s sslmode=disable", db_host, db_port, db_user, db_password, db_name)
 	db, err := sql.Open("postgres", psqconn)
 	if err != nil {
-		log.Fatal("Can not accessto DB", err)
+		log.Fatal("Can not access to DB", err)
 	}
 	return db
 }
 
-func insert_url(short, long string, db *sql.DB) error{
-	_, err := db.Exec(fmt.Sprintf("INSERT INTO records (long_url, short_url) VALUES (%s, %s)", long, short))
+func insert_url(short_url, long_url string, db *sql.DB) error{
+	_, err := db.Exec(fmt.Sprintf("INSERT INTO records (long_url, short_url) VALUES ('%s', '%s')", long_url, short_url))
 	if err != nil {		
 		return err
 	}
 	return nil
+}
+
+func if_data_exists(flag *bool,long_url string, db *sql.DB) (error){
+	record, err := db.Query(fmt.Sprintf("SELECT EXISTS(SELECT 1 FROM records where long_url = '%s')", long_url))
+	if err != nil{
+		return  err
+	}
+	for record.Next(){
+		err = record.Scan(flag)
+		if err != nil {
+			log.Fatal("Can not check if data exists", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func get_short_url(long_url string, db *sql.DB) (string, error){
+	var res string
+
+	record, err := db.Query(fmt.Sprintf("SELECT short_url FROM records WHERE long_url = ('%s')", long_url))
+	if err != nil{
+		return res, err
+	}
+	for record.Next(){
+		err = record.Scan(&res)
+		if err != nil{
+			return res, err
+		}
+	}
+	return res, nil
+}
+
+func get_long_url(short_url string, db *sql.DB) (string, error){
+	var res string
+
+	record, err := db.Query(fmt.Sprintf("SELECT long_url FROM records WHERE short_url = ('%s')", short_url))
+	if err != nil{
+		return res, err
+	}
+	for record.Next(){
+		err = record.Scan(&res)
+		if err != nil{
+			return res, err
+		}
+	}
+	return res, nil
 }
--- internal/handlers.go
+++ internal/handlers.go
@@ -8,40 +8,72 @@ import (
 	"net/http"
 )
 
-var Db *sql.DB
 
 
-func send_response(w http.ResponseWriter, short string){
+func send_response(w http.ResponseWriter, response interface{}){
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(short)
+	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		if_error_respose(w, errors.New("encoding error"), http.StatusInternalServerError)
 		return
 	}
-	
 }
 
 func Short_url(w http.ResponseWriter, r *http.Request){
-	Db = Db_connect()
-	log.Println("Connected to db")
-	defer Db.Close()
+	var if_short_exists bool
+	var url_req JsonUrl
+	var db *sql.DB = Db_connect()
+	var encoded_string JsonUrl
 
+	log.Println("Connected to db")
+	defer db.Close()	
 	if r.Method != "POST"{
 		if_error_respose(w, errors.New("only POST method"), http.StatusBadRequest)
-	}
-	var url_req JsonUrl
-	if err := decoder_json(&url_req, r.Body); err != nil{
-		if_error_respose(w, err, http.StatusBadRequest)
-	}
-	encoded_string := hash_func(url_req.Url)
-	if err := insert_url(encoded_string, url_req.Url, Db); err != nil{
-		if_error_respose(w, errors.New("data base adding error"), http.StatusInternalServerError)
 		return
 	}
-	log.Print(url_req.Url, " added to db with shortlink ", encoded_string, "\n")
-	send_response(w, encoded_string)
+	if err := decoder_json(&url_req, r.Body); err != nil{	
+		if_error_respose(w, err, http.StatusBadRequest)
+	}
+	if err_db := if_data_exists(&if_short_exists, url_req.Url, db);err_db != nil{
+		if_error_respose(w, errors.New("db check for existence error"), http.StatusInternalServerError)
+		return
+	}
+	if if_short_exists{
+		var err_response error
+		if encoded_string.Url, err_response = get_short_url(url_req.Url, db); err_response != nil{
+			if_error_respose(w, errors.New("getting short url from db error"), http.StatusBadRequest)
+		}
+		log.Println("encoded string has been sent")
+		send_response(w, encoded_string)	
+	} else {
+			encoded_string.Url = hash_func(url_req.Url)
+		if err := insert_url(encoded_string.Url, url_req.Url, db); err != nil{
+			if_error_respose(w, errors.New("db adding data error"), http.StatusInternalServerError)
+			return
+		}
+		log.Print(url_req.Url, " added to db with shortlink ", encoded_string, "\n")
+		
+		send_response(w, encoded_string)
+	}
 }
 
 func Long_url(w http.ResponseWriter, r *http.Request){
+	var url_req JsonUrl
+	var decoded_string JsonUrl
+	var db *sql.DB = Db_connect()
+	var err error
 
+	defer db.Close()
+	if r.Method != "GET"{
+		if_error_respose(w, errors.New("only GET method"), http.StatusBadRequest)
+		return
+	}
+	if err_json := decoder_json(&url_req, r.Body); err_json != nil{	
+		if_error_respose(w, err_json, http.StatusBadRequest)
+	}
+	decoded_string.Url, err = get_long_url(url_req.Url, db)
+	if err != nil{
+		if_error_respose(w, errors.New("getting long url from db error"), http.StatusBadRequest)
+	}
+	send_response(w, decoded_string)
 }
