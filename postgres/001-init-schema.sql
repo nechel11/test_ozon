98003cd7d035402e4b2963e0d807d7292ec15781
nechel11 <zaf-khasanov@yandex.ru>
2022-07-28 13:45:05 +0300

    a

--- postgres/001-init-schema.sql
+++ postgres/001-init-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS records
 (
 	records_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-	long_url text
-	short_url character varying(10)
+	long_url text,
+	short_url character varying(10),
 	CONSTRAINT records_pkey PRIMARY KEY (records_id)
 );
